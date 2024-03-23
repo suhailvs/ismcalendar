969014cfa2c688cafa2f87f43f021f8eb8600336
suhailvs <suhailvs@gmail.com>
2024-03-23 12:10:22 +0530

    added events

--- app/events.tsx
+++ app/events.tsx
@@ -1,36 +1,34 @@
 const now = new Date()
 
 export default [
-  /* {
-    id: 0,
-    title: 'All Day Event very long title',
-    allDay: true,
-    start: new Date(2015, 3, 0),
-    end: new Date(2015, 3, 1),
-  }, */
-  {
-    id: 1,
-    title: 'Long Event',
-    start: new Date(2024, 3, 25),
-    end: new Date(2024, 3, 26),
-  },
   {
     id: 2,
-    title: 'Meeting',
-    start: new Date(2024, 3, 24, 10, 30, 0, 0),
-    end: new Date(2024, 3, 24, 12, 30, 0, 0),
-    desc: 'Pre-meeting meeting, to prepare for the meeting',
+    title: 'QHLS Model Exam',
+    start: new Date(2024, 2, 24, 9, 0, 0, 0),
+    end: new Date(2024, 2, 24, 10, 0, 0, 0),
   },
-  {
-    id: 3,
-    title: 'Today',
-    start: new Date(new Date().setHours(new Date().getHours() - 3)),
-    end: new Date(new Date().setHours(new Date().getHours() + 3)),
-  },
+  // {
+  //   id: 3,
+  //   title: 'Today',
+  //   start: new Date(new Date().setHours(new Date().getHours() - 3)),
+  //   end: new Date(new Date().setHours(new Date().getHours() + 3)),
+  // },
   {
     id: 4,
-    title: 'Point in Time Event',
-    start: now,
-    end: now,
+    title: 'Athani Salafi Masjid Ramadan Speech',
+    start: new Date(2024, 2, 24, 16, 30, 0, 0),
+    end: new Date(2024, 2, 24, 18, 15, 0, 0),
+  },
+  {
+    id: 5,
+    title: 'Athani Salafi Masjid Ramadan Speech',
+    start: new Date(2024, 2, 31, 16, 30, 0, 0),
+    end: new Date(2024, 2, 31, 18, 15, 0, 0),
+  },
+  {
+    id: 6,
+    title: 'Athani Salafi Masjid Ramadan Speech',
+    start: new Date(2024, 3, 7, 16, 30, 0, 0),
+    end: new Date(2024, 3, 7, 18, 15, 0, 0),
   },
 ]
--- app/page.tsx
+++ app/page.tsx
@@ -9,21 +9,38 @@ import events from './events';
 export default function Home() {
   // const [events, setEvents] = useState([]);
   const localizer = useMemo(() => momentLocalizer(moment), []);
-  const handleCalendarNavigate = (date: Date) => {
-    console.log("Month ----->>>", moment(date).format("MMMM YYYY"));
-  };
+  // const handleCalendarNavigate = (date: Date) => {
+  //   console.log("Month ----->>>", moment(date).format("MMMM YYYY"));
+  // };
+  // const {views, ...otherProps} = useMemo(() => ({
+  //     views: {
+  //         month: true,       
+  //     }
+  // }), [])
   return (
     <>
       <div className="row custom_row bg-white rounded-corner h-100 p-4">
         <div className="col-md-12 px-0">
           <Calendar
             localizer={localizer}
+            // views={views}
             events={events}
             style={{ height: 580 }}
-            onNavigate={handleCalendarNavigate}
+            // onNavigate={handleCalendarNavigate}
+            defaultView="day"
           />
         </div>
       </div>
     </>
   );
 }
+
+/*
+ initialView: 'dayGridWeek',
+  headerToolbar: {
+    left: 'prev,next',
+    center: 'title',
+    right: 'dayGridMonth,dayGridWeek,dayGridDay' // user can switch between the two
+  },
+    height: '100%',
+    */
